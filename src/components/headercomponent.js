3dbc60e780afe9a4166dc420b270dc35fe75aefb
gangahasan <rgangabhavani2@gmail.com>
2024-12-03 10:44:41 +0530

    heading change

--- src/components/headercomponent.js
+++ src/components/headercomponent.js
@@ -29,37 +29,5 @@ const TopNavbar = () => {
 
 export default TopNavbar;
 
-// import React from "react";
-// import"../styles/header.css";
-// import { IoMdRefresh } from "react-icons/io";
-// import { MdOutlineEdit } from "react-icons/md";
-// import { MdOutlineDeleteOutline } from "react-icons/md";
 
-// const Topnavbar = (e) =>{
 
-//     return (
-//       <div>
-//         <div className="buttons">
-//           <button className="back">BACK</button>
-//           <div className="edit-and-all">
-//             <button>
-//               <MdOutlineEdit color="blue" style={{ verticalAlign: "middle" }} />{" "}
-//               EDIT
-//             </button>
-//             <button>
-//               <MdOutlineDeleteOutline
-//                 color="red"
-//                 style={{ verticalAlign: "middle" }}
-//               />{" "}
-//               DELETE
-//             </button>
-//             <button>
-//               <IoMdRefresh color="blue" style={{ verticalAlign: "middle" }} />{" "}
-//               REFRESH
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     );
-// }
-// export default Topnavbar;
